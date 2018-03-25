178b98e01333726e16a83f11cf84203d658898b9
zul.kawsar <zul.kawsar@gmail.com>
2018-03-25 12:22:49 +0600

    route model controller

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// ================= Buyer ====================
+Route::resource('buyer', 'Buyer\BuyerController', ['only' => [ 'index', 'show' ]] );
+
+
+// ================= Seller ====================
+Route::resource('seller', 'Seller\SellerController', ['only' => [ 'index', 'show' ]] );
+
+
+// ================= Category ====================
+Route::resource('category', 'Category\CategoryController', ['except' => [ 'create', 'edit' ]] );
+
+
+// ================= Transaction ====================
+Route::resource('transaction', 'Transaction\TransactionController', ['only' => [ 'index', 'show' ]] );
+
+
+// ================= Product ====================
+Route::resource('product', 'Product\ProductController', ['only' => [ 'index', 'show' ]] );
+
+
+// ================= User ====================
+Route::resource('user', 'User\UserController', ['except' => [ 'create', 'edit' ]] );
