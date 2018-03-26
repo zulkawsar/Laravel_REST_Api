2696a3dd150bfca2635986b7477f17a464342639
zul.kawsar <zul.kawsar@gmail.com>
2018-03-26 21:54:53 +0600

    seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::turncate();
+        Category::turncate();
+        Product::turncate();
+        Transaction::turncate();
+        DB::table('category_product')->turncate();
+
+
+        $userQuantiry = 200;
+        $categoryQuantiry = 20;
+        $productQuantiry = 1000;
+        $transactionQuantiry = 1000;
+
+        factory(User::class, $userQuantiry )->create();
+        factory(Category::class, $categoryQuantiry )->create();
+        factory(Product::class, $productQuantiry )->create()->each(
+        	function($product){
+        		$categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+
+        		$product->categories()->attach($categories);
+        });
+        factory(Transaction::class, $transactionQuantiry )->create();
     }
 }
