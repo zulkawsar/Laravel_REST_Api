cd231b55acfc455a9f386cb063013214a0852201
zul.kawsar <zul.kawsar@gmail.com>
2018-03-26 21:32:13 +0600

    factory done

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Faker\Generator as Faker;
 
 /*
@@ -13,11 +17,46 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
+
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'verified' => $verified = $Faker->randomElement([ User::VERIFIED_USER , User::UNVERIFIED_USER ]),
+        'verified_tocken' => $verified == User::VERIFIED_USER ? null : User::generatedVerificationCode(),
+        'admin' => $verified = $Faker->randomElement([ User::ADMIN_USER , User::REGULAR_USER ]),
+
+    ];
+});
+
+$factory->define(Category::class, function (Faker $faker) {
+    return [
+    	'name' => $faker->word,
+    	'description' => $faker->paragraph(1),
+
+    ];
+});
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+    	'name' => $faker->word,
+    	'description' => $faker->paragraph(1),
+    	'quantity' => $faker->numberBetween(1, 10),
+    	'status' => $faker->randomElement([ Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT ]),
+    	'image' => $faker->randomElement( '1.jpg', '2.jpg' ),
+    	'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker $faker) {
+	$seller = Seller::has('product')->get()->random();
+	$buyer = User::all()->except($seller->id)->random();
+    return [
+    	'quantity' => $faker->numberBetween(1, 3),
+    	'buyer_id' => $buyer->id,
+    	'product_id' => $seller->products->random()->id,
+
     ];
 });
