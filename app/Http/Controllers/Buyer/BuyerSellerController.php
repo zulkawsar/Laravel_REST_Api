2d7ca8198c1cf34858573368f59b6dc036d04439
zul.kawsar <zul.kawsar@gmail.com>
2018-04-07 02:23:37 +0600

    buyer complex query

---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $buyerSeller = $buyer->transactions()->with('product.seller')
+            ->get()
+            ->pluck('product.seller')
+            ->unique('id')
+            ->values();
+        return $this->showAll($buyerSeller);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buyer $buyer)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,8 @@ use Illuminate\Http\Request;
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => [ 'index', 'show' ]] );
 Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => [ 'index']] );
 Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => [ 'index']] );
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => [ 'index']] );
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => [ 'index']] );
 
 
 // ================= Seller ====================
